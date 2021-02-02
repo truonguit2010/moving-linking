d5abcaa6d0fb61514a0ed15ee23b9ec709a04454
Pham Son Truong <truongps@sendo.vn>
2021-02-02 15:23:32 +0700

    - Update related resource folder.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,13 +4,13 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "91b73081cdd0a97065893cbffaab4294",
-"index.html": "2892bb8a09d50c18da337acbb12dd7bc",
-"/": "2892bb8a09d50c18da337acbb12dd7bc",
+"index.html": "c0d9b6ff332a30eb4b39b7eff8150250",
+"/": "c0d9b6ff332a30eb4b39b7eff8150250",
 "main.dart.js": "0154d8cae6aea09cc8a9da511d8bbea7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "71276a8ba7d1512c5f36706f1d5c19a6",
+"manifest.json": "4e947d37fcd4bd9177f8256f1459841c",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "f82c373b55fffa2f674a92d36a2c6519",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
