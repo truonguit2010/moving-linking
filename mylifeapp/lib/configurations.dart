4e85cc7b58fff056a75415b5b2df9849af16dc46
Pham Son Truong <truongps@sendo.vn>
2021-02-04 17:41:35 +0700

    - Add Awesome font for icons.

---
+++ mylifeapp/lib/configurations.dart
@@ -0,0 +1,4 @@
+const String LINK_GITHUB = '';
+const String LINK_APPSTORE = '';
+const String LINK_GOOGLEPLAY = '';
+const String EMAIL_PERSIONAL = 'truonguit2010@gmail.com';
--- mylifeapp/lib/widgets/footer/footer.dart
+++ mylifeapp/lib/widgets/footer/footer.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:js' as js;
+
+import 'package:mylifeapp/configurations.dart';
 
 class Footer extends StatelessWidget {
   @override
@@ -18,39 +22,126 @@ class Footer extends StatelessWidget {
 class _MenuColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      child: Column(
+        children: [
+//          _Name()/
+        ],
+      ),
+    );
   }
 }
 
 class _Name extends StatelessWidget {
+  final String text;
+
+  final _textStyle = TextStyle(color: Colors.white, fontSize: 24);
+
+  _Name(this.text);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Text(
+      text,
+      style: _textStyle,
+    );
   }
 }
 
 class _MenuItem extends StatelessWidget {
+  final String text;
+
+  final _textStyle = TextStyle(color: Colors.white, fontSize: 24);
+
+  _MenuItem(this.text);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Text(
+      text,
+      style: _textStyle,
+    );
   }
 }
 
 class _LinkedColumn extends StatelessWidget {
+  TextStyle _textStyle = TextStyle(color: Colors.white, fontSize: 12);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [_GidHub(), _AppStore(), _GooglePlay()],
+            ),
+          ),
+          Container(
+            child: FlatButton(
+              child: Text(
+                EMAIL_PERSIONAL,
+                style: _textStyle,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _GidHub extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: FaIcon(FontAwesomeIcons.github),
+        onPressed: () {
+          js.context.callMethod('open', [LINK_GITHUB]);
+        });
+  }
+}
+
+class _AppStore extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: FaIcon(FontAwesomeIcons.appStore),
+        onPressed: () {
+          js.context.callMethod('open', [LINK_APPSTORE]);
+        });
+  }
+}
+
+class _GooglePlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: FaIcon(FontAwesomeIcons.googlePlay),
+        onPressed: () {
+          js.context.callMethod('open', [LINK_GOOGLEPLAY]);
+        });
   }
 }
 
 class _BioColumn extends StatelessWidget {
+  String get bioText {
+    return 'Xander Gottlieb is a Designer and Developer specialising in User Experience. His portfolio includes more than 50 websites and several cross-platfrom apps.';
+  }
+
+  TextStyle _textStyle = TextStyle(color: Colors.white, fontSize: 12);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            bioText,
+            style: _textStyle,
+          )
+        ],
+      ),
+    );
   }
 }
